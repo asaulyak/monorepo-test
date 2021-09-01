39b558d6718991c5fa9e2126f52096d53539e09d
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2021-09-01 09:23:30 +0300

    update ci script

--- ci/build-server.sh
+++ ci/build-server.sh
@@ -23,7 +23,7 @@ for app in "${apps_array[@]}";do
 done
 
 for app in "${apps_array[@]}";do
-  cd ./release/$app && zip -r $app.zip ./ && mv $app.zip ../ && rm -rf ./$app && cd -
+  cd ./release/$app && zip -r $app.zip ./ && mv $app.zip ../ && cd - && rm -rf ./$app
 done
 
 echo "Finished server applications build"
