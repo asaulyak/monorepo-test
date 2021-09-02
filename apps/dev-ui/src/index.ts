77584a7e954a25d9227c87879ad2a74eea3f7800
Eugene Asaulyak <jenya.asaulyak@gmail.com>
2021-09-02 19:49:07 +0300

    add tests

--- apps/dev-ui/src/index.ts
+++ apps/dev-ui/src/index.ts
@@ -1,13 +1,4 @@
 import {guid} from '@oapi-mono/uuid';
-import {DbService, User} from '@oapi-mono/orm';
 
-(async () => {
-  DbService.create();
-  const users = (await User.find()) as User[];
-  console.log('users');
+console.error('Started dev-ui server', guid());
 
-  users.forEach(user => {
-    console.log(user.id);
-  });
-  console.error('Started dev-ui server', guid());
-})();
